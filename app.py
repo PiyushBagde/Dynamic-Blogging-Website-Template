e5a661697d8bec1e08a8663c3099688b1f572d4c
PiyushBagde <piyushbagde002@gmail.com>
2023-09-22 18:19:31 +0530

    added config.json

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import json
+
+
+with open("templates/config.json", 'r') as c:
+    params = json.load(c)["params"]
+
+local_server = True
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/staythere'
+
+if local_server:
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['prod_uri']
+
+
 db = SQLAlchemy(app)
+
+
 
 
 class Contacts(db.Model):
@@ -19,17 +34,17 @@ class Contacts(db.Model):
 
 @app.route('/')
 def home():  # put application's code here
-    return render_template('index.html')
+    return render_template('index.html', params=params)
 
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', params=params)
 
 
 @app.route('/post')
 def post():  # put application's code here
-    return render_template('post.html')
+    return render_template('post.html', params=params)
 
 
 @app.route("/contact", methods=['GET', 'POST'])
@@ -45,7 +60,7 @@ def contact():
         db.session.add(entry)
         db.session.commit()
 
-    return render_template('contact.html')
+    return render_template('contact.html', params=params)
 
 
 if __name__ == "__main__":
