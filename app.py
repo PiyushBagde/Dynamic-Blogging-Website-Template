5c7c979fe3d89d265adbcfedcb8b405c52e8044c
PiyushBagde <piyushbagde002@gmail.com>
2023-09-22 15:52:16 +0530

    connected to database and added dummy entries in the form

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 app = Flask(__name__)
-#
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/staythere'
-# db = SQLAlchemy(app)
-#
-# class Contacts(db.Model):
-#     sno = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(80), nullable=False)
-#     phone_num = db.Column(db.String(12), nullable=False)
-#     msg = db.Column(db.String(120), nullable=False)
-#     date = db.Column(db.String(12), nullable=True)
-#     email = db.Column(db.String(20), nullable=False)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/staythere'
+db = SQLAlchemy(app)
+
+
+class Contacts(db.Model):
+    # sno, name, email, phone_num. message, date
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    phone_num = db.Column(db.String(12), nullable=False)
+    message = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(12), nullable=True)
+    email = db.Column(db.String(20), nullable=False)
+
+
 @app.route('/')
 def home():  # put application's code here
     return render_template('index.html')
@@ -27,8 +32,19 @@ def post():  # put application's code here
     return render_template('post.html')
 
 
-@app.route('/contact')
-def contact():  # put application's code here
+@app.route("/contact", methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        # adding entry to the database
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        message = request.form.get('message')
+        print(request.form)
+        entry = Contacts(name=name, phone_num=phone, message=message, email=email, date=datetime.now())
+        db.session.add(entry)
+        db.session.commit()
+
     return render_template('contact.html')
 
 
