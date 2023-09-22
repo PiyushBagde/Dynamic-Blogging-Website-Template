8f17eb0cee845af13f6296679b0d47a870c56902
PiyushBagde <piyushbagde002@gmaill.com>
2023-09-22 13:09:19 +0530

    lap sync

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from flask import Flask, render_template
-
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/staythere'
+db = SQLAlchemy(app)
 
+class Contacts(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    phone_num = db.Column(db.String(12), nullable=False)
+    msg = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(12), nullable=True)
+    email = db.Column(db.String(20), nullable=False)
 @app.route('/')
 def home():  # put application's code here
     return render_template('index.html')
@@ -23,4 +32,5 @@ def contact():  # put application's code here
     return render_template('contact.html')
 
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
