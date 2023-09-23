ddd5d8173a9a9ff6b272407f80bcb417db9e5a02
PiyushBagde <piyushbagde002@gmaill.com>
2023-09-23 13:31:41 +0530

    removed unnecessary stuff

--- app.py
+++ app.py
@@ -13,9 +13,6 @@ local_server = True
 app = Flask(__name__)
 
 app.config.update(
-    # MAIL_SERVER='smtp.gmail.com',
-    # MAIL_PORT='465',
-    # MAIL_USE_SSL=True,
     MAIL_SERVER='smtp.gmail.com',
     MAIL_PORT='465',
     MAIL_USE_SSL=True,
