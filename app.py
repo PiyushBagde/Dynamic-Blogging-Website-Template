16147292818953282008b63eb73302e95a07dfb0
PiyushBagde <piyushbagde002@gmail.com>
2023-09-26 21:52:34 +0530

    added limits on post on home page

--- app.py
+++ app.py
@@ -46,11 +46,13 @@ class Posts(db.Model):
     img_file = db.Column(db.String(12), nullable=True)
     date = db.Column(db.String(12), nullable=True)
     slug = db.Column(db.String(21), nullable=False)
+    tagline = db.Column(db.String(120), nullable=False)
 
 
 @app.route('/')
 def home():
-    return render_template('index.html', params=params)
+    posts = Posts.query.filter_by().all()[0:params['no_of_posts']]
+    return render_template('index.html', params=params, posts=posts)
 
 
 @app.route('/about')
